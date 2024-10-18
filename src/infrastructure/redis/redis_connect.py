72d95d6567fe25b39f8dae4375b035d1aff64a4c
SkaDin <ga.akam@yandex.ru>
2024-10-18 16:22:08 +0300

    refactor migrations path

--- src/infrastructure/redis/redis_connect.py
+++ src/infrastructure/redis/redis_connect.py
@@ -1,11 +1,3 @@
-# import redis.asyncio as aioredis
-#
-# from src.core.config import config
-#
-# redis = aioredis.from_url(
-#     config.REDIS_URL,
-#     decode_responses=True,
-# )
 from typing import AsyncGenerator
 
 from redis.asyncio import ConnectionPool, Redis
@@ -13,7 +5,6 @@ from redis.asyncio import ConnectionPool, Redis
 from src.core.config import config
 
 
-# Функция инициализации пула соединений
 def init_redis_pool() -> ConnectionPool:
     return ConnectionPool.from_url(
         config.REDIS_URL,
@@ -21,12 +12,10 @@ def init_redis_pool() -> ConnectionPool:
     )
 
 
-# Асинхронная функция для закрытия пула соединений
 async def close_redis_pool(redis_pool: ConnectionPool) -> None:
     await redis_pool.disconnect()
 
 
-# Асинхронный генератор для получения объекта Redis из пула
 async def get_redis_session() -> AsyncGenerator[Redis, None]:
     redis_pool = init_redis_pool()
     redis = Redis(connection_pool=redis_pool)
